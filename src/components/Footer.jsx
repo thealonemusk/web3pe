67cd61c1a2de4191ca7c111a2803a8b37403881c
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2023-10-16 14:02:56 +0530

    fixed the typo in the front page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className='sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5' />
       <div className='sm:w-[90%] w-full flex justify-between items-center mt-3'>
         <p className='text-white text-sm text-center'>
-          @Cryptmo 2023
+          @Web3pe 2023
         </p>
         <p className='text-white text-sm text-center'>
           All Rights Reserved
